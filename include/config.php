7ae973f126e80d6df4e7491208b1a0885ed7ab48
ouzhigang <ouzhiganggba@gmail.com>
2018-08-22 15:05:31 +0800

    update config

--- include/config.php
+++ include/config.php
@@ -1,9 +1,9 @@
 <?php
 date_default_timezone_set("Etc/GMT-8");
 
-define("dbhost","");
-define("dbuser","");
-define("dbpass","");
-define("dbname","");
+define("dbhost","prohost");
+define("dbuser","prouser");
+define("dbpass","propass");
+define("dbname","prodbname");
 $conn = mysqli_connect(dbhost,dbuser,dbpass,dbname);
 mysqli_query($conn, 'set names utf8');
